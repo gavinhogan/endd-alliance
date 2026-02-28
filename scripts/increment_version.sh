75daa2ff52b17d706c6561b35490a466cec9a031
Gavin Hogan <gavinhogan@gmail.com>
2026-02-28 18:33:09 -0500

    Sync HTML footers to v1.0.3

--- scripts/increment_version.sh
+++ scripts/increment_version.sh
@@ -2,10 +2,10 @@
 
 # Read current version
 VERSION_FILE="/Users/gavinhogan/Documents/confuzed/VERSION"
-VERSION=$(cat "$VERSION_FILE")
+OLD_VERSION=$(cat "$VERSION_FILE")
 
 # Split version into parts
-IFS='.' read -r major minor patch <<< "$VERSION"
+IFS='.' read -r major minor patch <<< "$OLD_VERSION"
 
 # Increment patch version
 patch=$((patch + 1))
@@ -14,4 +14,8 @@ patch=$((patch + 1))
 NEW_VERSION="$major.$minor.$patch"
 echo "$NEW_VERSION" > "$VERSION_FILE"
 
-echo "Version incremented to $NEW_VERSION"
+# Sync with HTML files
+# Assuming version is stored as "VERSION: x.x.x" in footers
+grep -rli "VERSION: " . --include="*.html" | xargs -I@ sed -i '' "s/VERSION: $OLD_VERSION/VERSION: $NEW_VERSION/g" @
+
+echo "Version incremented from $OLD_VERSION to $NEW_VERSION and synced to HTML."
